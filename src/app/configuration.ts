7240705fcbba365cced5e18888f528520bd2aa76
ajaysharma92 <sharmajay121@gmail.com>
2019-01-13 23:58:10 +0530

    V1.5 - Make Tab 2 Dynamic

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -4,5 +4,15 @@ export const configuration={
         headingText:'Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects, enjoy!',
         buttonText:'START CREATING TODAY',
         buttonLink:'\home'
+    },
+    intro:{
+        introHead: 'How We Help You To Sell Your Product',
+        headPara: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+        features: [
+            {icon: 'html5', title:'HTML5 &#38; CSS3', paragraph:'Has ne tritani atomorum conclusionemque, in dolorum volumus cotidieque eum. At vis choro neglegentur iudico'},
+            {icon: 'bolt', title:'Easy to Use', paragraph:'Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sit detracto mediocrem disputationi'},
+            {icon: 'tablet', title:'Fully Responsive', paragraph:'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro nemore vivendum'},
+            {icon: 'rocket', title:'Parallax Effect', paragraph:'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro nemore vivendum'},
+        ]
     }
 }
